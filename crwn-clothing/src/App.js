b230801ecb856a8a2431f7c0785cd894718226e7
skiran017 <skiran017@gmail.com>
2021-03-21 14:18:21 +0530

    shop-page, collection-preview, collection-item components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./page/homepage/homepage.component";
+import ShopPage from "./page/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
