7c4f1290dcc02d96304738c1ae3129ccdbcadf70
skiran017 <skiran017@gmail.com>
2021-04-02 01:11:40 +0530

    added collection overview component, data normalization

--- crwn-clothing/src/components/collections-overview/collections-overview.component.jsx
+++ crwn-clothing/src/components/collections-overview/collections-overview.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
-import CollectionPreview  from "../collection-preview/collection-preview.component";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
 import "./collections-overview.styles.scss";
 
@@ -16,6 +16,6 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 export default connect(mapStateToProps)(CollectionsOverview);
